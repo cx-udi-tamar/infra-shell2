0ace5f148cf17262de45beba7ba6025964ed4845
Udi-Yehuda Tamar <udi.tamar@skyhawk.security>
2025-02-26 22:53:32 +0200

    Set global path to .config

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env zsh
 # set -e
+
+export quiet_flag="false"
+export stdout_flag="true"
+
+
 NAME_BASE_REPO="infra-shell2"
 export PATH_BASE_REPO="${HOME}/git/${NAME_BASE_REPO}"
 export PATH_BASE_REPO_SRC="${HOME}/git/${NAME_BASE_REPO}/src"
 
-export DIR_DOTFILES=/tmp/.dotfiles
+# export DIR_DOTFILES=/tmp/.dotfiles
+export DIR_DOTCONF=${HOME}/.config
 
-export WEZTERM_CONFIG_FILE=${DIR_DOTFILES}/wezterm.lua
+export WEZTERM_CONFIG_FILE=${DIR_DOTCONF}/wezterm/wezterm.lua
 
-dotbot -c ${PATH_BASE_REPO}/infra.yaml
+dotbot -c ${PATH_BASE_REPO}/install.conf.yaml
 
-source ${DIR_DOTFILES}/.zshrc
+echo "INSTALLATION COMPLETED"
